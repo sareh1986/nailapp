0b571d8b9d6f989acba9a0e47cb8bdaf9242234b
sareh1986 <51043031+sareh1986@users.noreply.github.com>
2020-08-15 15:06:34 +0430

    api
    api

--- src/Components/moreInfo2.js
+++ src/Components/moreInfo2.js
@@ -1,18 +1,47 @@
-import React , {Component} from 'react'
+import React, { Component } from "react";
 
+export default class moreInfo2 extends Component {
+  // my-json-server.typicode.com/user/repo/posts/1
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+  componentDidMount() {
+    fetch("my-json-server.typicode.com/user/repo/posts/1")
+      .then(res => res.json())
+      .then(
+        result => {
+          this.setState({
+            isLoaded: true,
+            items: result.items
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
+  }
 
-
-export default class moreInfo2 extends Component{
-
-    render(){
-        return(
-            <div>
-            <p>my phone number is:</p>
-            <p>tel : 0912-------</p>
-            </div>
-
-
-        )
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if(error){
+        return <div>Error: ----</div>;
     }
-
+    return (
+      <div>
+        <p>name: {items.user}</p>
+        <p>url: {items.url}</p>
+      </div>
+    );
+  }
 }
